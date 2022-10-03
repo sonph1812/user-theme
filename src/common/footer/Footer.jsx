78a12ebefbbd9c372fd4c550ad160d0e1c86b77c
sonph <sonph>
2022-10-03 09:11:16 +0700

    son

---
+++ src/common/footer/Footer.jsx
@@ -0,0 +1,58 @@
+import React from "react"
+import "./style.css"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className='container grid2'>
+          <div className='box'>
+            <h1>Kim Long</h1>
+            <p>Download app: </p>
+            <div className='icon d_flex'>
+              <div className='img d_flex'>
+                <i class='fa-brands fa-google-play'></i>
+                <span>Google Play</span>
+              </div>
+              <div className='img d_flex'>
+                <i class='fa-brands fa-app-store-ios'></i>
+                <span>App Store</span>
+              </div>
+            </div>
+          </div>
+
+          <div className='box'>
+            <h2>About Us</h2>
+            <ul>
+              <li>Careers</li>
+              <li>Our Stores</li>
+              <li>Our Cares</li>
+              <li>Terms & Conditions</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </div>
+          <div className='box'>
+            <h2>Customer Care</h2>
+            <ul>
+              <li>Help Center </li>
+              <li>How to Buy </li>
+              <li>Track Your Order </li>
+              <li>Corporate & Bulk Purchasing </li>
+              <li>Returns & Refunds </li>
+            </ul>
+          </div>
+          <div className='box'>
+            <h2>Contact Us</h2>
+            <ul>
+              <li>40 Nguyễn Huy Tưởng, Thanh Xuân, Hà Nội</li>
+              <li>Email: sonkdqte@gmail.com</li>
+              <li>Phone: 0912345678</li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
